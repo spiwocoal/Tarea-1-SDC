2696f5c1164957ee88e4640cab1dc72a2617ac65
Bruno Pacheco <spiwocoal@gmail.com>
2024-09-18 23:16:39 -0300

    Agregar codigo simulaciones pregunta d

---
+++ matlab/graficos_d.m
@@ -0,0 +1,51 @@
+sims_pregunta_d;
+
+psi   = x_con(1,:) * 180/pi;
+dpsi  = x_con(2,:) * 180/pi;
+omega = x_con(3,:) * 60/(2*pi);
+
+%% Graficar simulación
+f1 = figure(1);
+
+plot(t, psi, 'r', t, psi_d(t), 'r--');
+title("Ángulo de referencia y la salida");
+xlabel("Tiempo (s)", "Interpreter", "latex");
+ylabel("$\psi$ (deg)", "Interpreter", "latex");
+
+f2 = figure(2);
+
+subplot(2,2,1);
+plot(t, w_con);
+title("Entrada al actuador");
+xlabel("Tiempo (s)", "Interpreter", "latex");
+ylabel("$w$", "Interpreter", "latex");
+
+subplot(2,2,2);
+plot(t, v_icon);
+title("$v_{i}(t)$", "Interpreter", "latex");
+xlabel("Tiempo (s)", "Interpreter", "latex");
+ylabel("$v_{i}(t)$ (V)", "Interpreter", "latex");
+
+subplot(2,2,3);
+plot(t, dpsi);
+title("$\dot{\psi}(t)$", "Interpreter", "latex");
+xlabel("Tiempo (s)", "Interpreter", "latex");
+ylabel("$\dot{\psi}(t)$ (deg/s)", "Interpreter", "latex");
+
+subplot(2,2,4);
+plot(t, omega);
+title("$\omega(t)$", "Interpreter", "latex");
+xlabel("Tiempo (s)", "Interpreter", "latex");
+ylabel("$\omega(t)$ (RPM)", "Interpreter", "latex");
+
+if ~exist('exportar', 'var')
+  exportar = false;
+end
+
+if exportar
+  matlab2tikz('figurehandle', f1, 'width', '0.9\textwidth', 'height', '0.3\textheight', ...
+    'interpretTickLabelsAsTex', true, './psi_pc.tex');
+  
+  matlab2tikz('figurehandle', f2, 'width', '0.9\textwidth', 'height', '0.6\textheight', ...
+    'interpretTickLabelsAsTex', true, './estado_pc.tex');
+end
--- matlab/resolucion_d.m
+++ matlab/resolucion_d.m
@@ -1,28 +1,22 @@
-%%Resolucion e
+%% Resolucion e
+parametros;
+pregunta_a;
 
-% Definición de parámetros
-r_b = 9.53e-3; r = 9.3e-3; g = 9.8;
-G = 17.5e-3; H = 67.5; m = 28.2e-3;
-R = 87.5e-3; b_b = 4.57e-6; tau = 0.66;
+syms k_a k_c k_st;
 
-syms b_0w a_1w a_0w b_1p b_0p a_2p a_1p a_0p ka kc k_st s;
+%% Lazo cerrado simbólico
+% FdT lazo directo
+h_ld = k_a * k_c * h_pv;
+% FdT lazo cerrado
+h_lc = h_ld / (1 + k_st * h_ld);
 
-% Lazo cerrado simbólico
-%psi/vi
-psi_vi = ((b_0w*(b_0p + b_1p*s))/((a_0w + a_1w*s)*(a_2p*s^2 + a_1p*s + a_0p)));
-
-%F_T lazo cerrado
-F_T = ((kc).*ka.*psi_vi*k_st)./(1 + (kc).*ka.*psi_vi.*k_st);
-
-[num,den] = numden(F_T);
-
-den_FT= collect(den,s);
-disp('Denominador FT:');
-disp(den_FT);
+[num, den] = numden(h_lc);
+disp('Denominador FdT:');
+disp(collect(den));
 
 %la parte derecha de la incuación la anotamos a mano 
 num_ineq = -(a_1p*a_1w*(a_0p*a_1w + a_1p*a_0w) + a_2p*a_0w*(a_0p*a_1w + a_1p*a_0w) - a_2p*a_1w*a_0p*a_0w);
-den_ineq = (a_1p*a_1w*ka*k_st*b_1p*b_0w) + (a_2p*a_0w*ka*k_st*b_1p*b_0w) - (a_2p*a_1w*ka*k_st*b_0p*b_0w);
+den_ineq = (a_1p*a_1w*k_a*k_st*b_1p*b_0w) + (a_2p*a_0w*k_a*k_st*b_1p*b_0w) - (a_2p*a_1w*k_a*k_st*b_0p*b_0w);
 
 % Calcular KC_final
 num_div_den = num_ineq /den_ineq;
@@ -32,8 +26,7 @@ pretty(num_div_den);
 %% Remplazo de a valores numericicos
 b_0w = G; a_1w = tau; a_0w = 1; b_1p = H*(2/5)*(r_b/r)^2;
 b_0p = H*b_b/(m*r^2); a_2p = (2/5)*(r_b/r)^2+1; a_1p = b_b/(m*r^2); a_0p = g/R;
-ka   = 100;   k_st  = 180/pi;
-
+k_a   = 100;   k_st  = 180/pi;
 
 KC_final= subs(num_div_den);
 disp('valor kc');
